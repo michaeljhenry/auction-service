c6740912284593fd6c83a50af96a86d3b6458bf6
Michael Henry <michaeljhenry@hotmail.ca>
2022-07-20 20:16:42 -0400

    getting auctions - filter by status

--- src/handlers/getAuctions.js
+++ src/handlers/getAuctions.js
@@ -3,14 +3,24 @@ import createError from 'http-errors';
 import commonMiddleware from '../lib/commonMiddleware';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient(); // this is static so its okay to define it here
-
+// query based on status
 async function getAuctions(event, context) {
     let auctions;
+    const { status } = event.queryStringParameters;
 
+    const params = {
+        TableName: process.env.AUCTIONS_TABLE_NAME,
+        IndexName: 'statusAndEndDate',
+        KeyConditionExpression: '#status= :status',
+        ExpressionAttributeValues: {
+            ':status': status
+        },
+        ExpressionAttributeNames: {
+            '#status': 'status'
+        }
+    }
     try {
-        const result = await dynamodb.scan({ 
-            TableName: process.env.AUCTIONS_TABLE_NAME 
-        }).promise(); // turn it into a promise
+        const result = await dynamodb.query(params).promise(); // turn it into a promise
         
         auctions = result.Items;
     } catch(error) {
